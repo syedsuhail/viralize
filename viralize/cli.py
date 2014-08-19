2d03dc9e3cd2da4145e237f435aa157d4fa4eecb
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-08-19 19:58:21 +0530

    Add mail sender and clean up directories

--- viralize/cli.py
+++ viralize/cli.py
@@ -12,19 +12,26 @@ def get_user_data(filename):
         config.read(filename)
     except:
         print "File does not exist"
-    dict1={}
     for section in config.sections():
+        dict1={}
+        dict1["channel"]=section
         options = config.options(section)
-        dict1["section"]=section
+        print dict1['channel']
         for option in options:
-            if config.get(section,option) == '':
-                print "In section %s, %s value is empty"%(section,option)
             try:
+                print config.get(section,option)
                 dict1[option] = config.get(section,option)
                 if dict1[option] == -1:
                     DebugPrint("skip: %s" % option)
+                if config.get(section,option) == '':
+                    print "In section %s, %s value is empty"%(section,option)
             except:
                 print("exception on %s!" %option)
                 dict1[option]=None
-            data.append(dict1)
+        data.append(dict1)
     return data
+
+
+
+
+            

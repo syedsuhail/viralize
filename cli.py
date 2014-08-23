62d8aca427a525dc6dca1e3a4e1df0f30ece814e
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-08-23 23:51:03 +0530

    Change cli.py location to root

---
+++ cli.py
@@ -0,0 +1,35 @@
+import ConfigParser
+import sys
+
+#filename = sys.argv[1]
+#filename='../scrap/viral.ini'
+
+def get_user_data(filename):
+    '''Gets relevant data from ini file and returns a list of dictionaries'''
+    config = ConfigParser.ConfigParser()
+    data=[]
+    try:
+        config.read(filename)
+    except:
+        print "File does not exist"
+    for section in config.sections():
+        dict1={}
+        dict1["channel"]=section
+        options = config.options(section)
+        for option in options:
+            try:
+                dict1[option] = config.get(section,option)
+                if dict1[option] == -1:
+                    DebugPrint("skip: %s" % option)
+                if config.get(section,option) == '':
+                    print "In section %s, %s value is empty"%(section,option)
+            except:
+                print("exception on %s!" %option)
+                dict1[option]=None
+        data.append(dict1)
+    return data
+
+
+
+
+            

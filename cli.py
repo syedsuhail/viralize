290d6bfeadc16aaf7fe061a4aa9f3cfd7eaaa9e1
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-08-29 22:01:57 +0530

    Modify cli.py
    Add status() to print status of each channel.
    Add main() which is main function to be run.
    Viralize is imported
    Remove show_info()

--- cli.py
+++ cli.py
@@ -1,17 +1,19 @@
 import ConfigParser
 import sys
-
-#filename = sys.argv[1]
-#filename='../scrap/viral.ini'
-
+import getpass
+from sys import argv
+from binascii import hexlify
+import viralize
+data_file = 'viral.ini'
 def get_user_data(filename):
     '''Gets relevant data from ini file and returns a list of dictionaries'''
     config = ConfigParser.ConfigParser()
     data=[]
     try:
         config.read(filename)
-    except:
+    except Exception:
         print "File does not exist"
+        return
     for section in config.sections():
         dict1={}
         dict1["channel"]=section
@@ -30,6 +32,27 @@ def get_user_data(filename):
     return data
 
 
+def get_username_pass(service):
+    username= hexlify(raw_input('Enter username for %s '%service))
+    password = hexlify(getpass.getpass('Password for %s $:'%service))
+    return username,password
 
+def status(results):
+    for key,value in results.iteritems():
+        print key,value
+    
+
+def main():
+    data=get_user_data(data_file)
+    results = viralize.viralize(data)
+    status(results)
+
+if __name__=='__main__':
+    try:
+        filename = argv[1]
+    except Exception:
+        print "Needs argument"
+        raise
+    get_user_data(filename)
 
             

a25f1d9e068e5f5d50c0ce373725d058cc4bf2b6
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-08-29 21:26:20 +0530

    Remove hardcoded data

--- viralize.py
+++ viralize.py
@@ -1,33 +1,40 @@
-import cli
+import glob
+from os.path import basename, splitext
+import importlib
+
+channels_dir='channels/'
+
+def get_channels(channels_dir):
+    channels={}
+    chan=[]
+    channel_files = glob.glob("{}/*.py".format(channels_dir))
+    for channel_file in channel_files:
+        if channel_file.endswith("__init__.py"):
+            continue
+        name, ext = splitext(basename(channel_file))
+        module_name = "channels.{}".format(name)
+        module = importlib.import_module(module_name)
+        chan.append(module)
+    return chan
+        #plugin = module.__plugin__(parser)
+        #plugins[module.__cname__] = plugin
 
 
-
-
-def viralize():
-    filename= cli.get_filename()
-    data=cli.get_user_data(filename)
-    channels=['Twitter','Facebook','Email','gmail','Wordpress']
+def viralize(data):
+    channels = get_channels(channels_dir)
     success={}
-    for dict1 in data:
-        if dict1['channel'] in channels:
-            if dict1['channel'] == 'Twitter':
-                import channels.twit as twit
-                success['Twitter']=twit.twitter_publish(dict1)
-            elif dict1['channel'] == 'Wordpress':
-                import channels.blog as blog
-                wp=blog.initialize()
-                success['Blog']=blog.wordpress_publish(dict1,wp)
-            elif dict1['channel']=='Facebook':
-                import channels.fb as fb
-                success['facebook'] = fb.fb_publish(dict1)
-            elif dict1['channel']=='Email':
-                from channels.smail import *
-                success['email'] = gmail(dict1)
-    
-                    
-        else:
-            print 'This app doesnt publish to %s'% dict1['channel']
-    print success
+    for channel in channels:
+        name=channel.__name__.split('.')[1].split('_')[0]
+        for d in data:
+            if d['channel'].lower() == name:
+                print 'doing %s' %name
+                try:
+                    res=getattr(channel,'publish')(d)
+                    success[name]= res
+                except Exception:
+                    print 'couldnt'
+                    continue
+    return success
 
 
 if __name__=='__main__':

f08fa650ba8970aab188ab326a52d45ba1154ad6
Rahul KP <36rahu@users.noreply.github.com>
2014-09-01 13:29:47 +0530

    Modified Contents

--- channels/wordpress_publish.py
+++ channels/wordpress_publish.py
@@ -1,56 +1,92 @@
-import ConfigParser
 import os
-import cli
-from getpass import getpass
-from binascii import hexlify, unhexlify
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import GetPosts, NewPost
+import ConfigParse
+import viralize
 
-def initialize():
-    MY_BLOG_CREDS = os.path.expanduser('.wordpress_credentials')
-    if not os.path.exists(MY_BLOG_CREDS):
-        blog_id = raw_input("Enter Blog addres (http://mysite.wordpress.com):")
-        blog_id = blog_id + "/xmlrpc.php"
-        username,password = cli.get_username_pass('Wordpress')
-        username = unhexlify(username)
-        password = unhexlify(password)
-        cfg = ConfigParser.RawConfigParser()
-        cfg.read('.my_blog_credentials')
-        cfg.add_section('Wordpress')
-        cfg.set('Wordpress', 'Blogaddres', blog_id)
-        cfg.set('Wordpress', 'Username', username)
-        cfg.set('Wordpress', 'Password', password)
-        with open('.wordpress_credentials', 'wb') as configfile:
-            cfg.write(configfile)
+Wordpress_credential = os.path.expanduser('.credentials')
+
+'''Returns the Wordpress address'''
+def get_value():
+    Wordpress_credential = os.path.expanduser('.credentials')
+    cfg = ConfigParser.RawConfigParser()
+    cfg.read(Wordpress_credential)
+    #Wordpress address taken from the credential file
+    if cfg.has_section('Wordpress'):
+        try:
+            Wordpress_id = cfg.get('Wordpress', 'Wordpressaddres')
+            password = cfg.get('Wordpress', 'password')
+            username = cfg.get('Wordpress', 'username')
+            Wordpress_id = Wordpress_id + "/xmlrpc.php"
+            return Wordpress_id,username,password
+        except ConfigParser.NoOptionError:
+            raise Error
     else:
-        cfg = ConfigParser.RawConfigParser()
-        cfg.read('.wordpress_credentials')
-        if cfg.has_section('Wordpress'):
-            blog_id = cfg.get('Wordpress', 'Blogaddres')
-            username = unhexlify(cfg.get('Wordpress', 'Username'))
-            password = unhexlify(cfg.get('Wordpress', 'Password'))
+        Wordpress_id = viralize.get_data('Wordpress address')
+        username = viralize.get_data('Wordpress username')
+        password = viralize.get_data('Wordpress password')
+        Wordpress_id = Wordpress_id + "/xmlrpc.php"
+        return Wordpress_id,username,password
+
+        
+
+def initialise():
+    cfg = ConfigParser.RawConfigParser()
+    
+    #Creates if there no exist an credential file 
+    if not os.path.exists(Wordpress_credential):
+        cfg.read(Wordpress_credential)
+        cfg.add_section('Wordpress')
+        Wordpress_id,username, password = get_value()
+        cfg.set('Wordpress', 'Wordpressaddres', username)
+        cfg.set('Wordpress', 'username', username)
+        cfg.set('Wordpress', 'password', password)
+        with open('.credentials', 'wb') as configfile:
+            cfg.write(configfile)
+    #Read from the credential file
+    else:
+        cfg.read(Wordpress_credential)
+	#Put Values in to the credential file
+        if not cfg.has_section('Wordpress'):
+            cfg.add_section('Wordpress')
+            Wordpress_id,username, password = get_value()
+            cfg.set('Wordpress', 'Wordpressaddres', Wordpress_id)
+            cfg.set('Wordpress', 'username', username)
+            cfg.set('Wordpress', 'password', password)
+            with open('.credentials', 'wb') as configfile:
+            cfg.write(configfile)
+        else:
+	    #Takes values from the credential file
+            Wordpress_id = cfg.get('Wordpress', 'Wordpressaddres')
+            username = cfg.get('Wordpress', 'username')
+            password = cfg.get('Wordpress', 'password')
     try:
-        wp = Client(blog_id, username, password)
+	#creates an wordpress object
+        wp = Client(Wordpress_id, username, password)
         return wp
     except Exception:
-        cli.show_info('could not authenticate')
-        return None
+        return 'could not authenticate'
+
+
 
     
-
-def publish(data):
+def publish():
     wp=initialize()
-    if wp == None:
-        return None
     post = WordPressPost()
-    post.title = data['tittle']
-    post.content = data['message']
+    #Checks tittle is exist
+    if not data['tittle']:
+        return 'The tittle could not create as empty'
+    else:
+        post.title = data['tittle']
+    #checks message is exist
+    if not data['message']:
+        return 'The message could not create as empty'
+    else:
+        post.content = data['message']
     post.terms_names = {'post_tag': ['test', 'firstpost'],'category': ['Introductions', 'Tests']}
     post.post_status = 'publish'
+    #trying in to post on the wordpress
     try:
         wp.call(NewPost(post))
-        cli.show_info( "Published the message in wordpress succesfully..")
-        return 1
+        return 'Published the message in wordpress succesfully..'
     except Exception:
-        cli.show_info("Could not publish")
-        return None
+        return 'Could not publish'
+'

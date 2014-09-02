c264447d37a60a26405ac501c969b4568f3929bf
syedsuhail <ssuhail.ahmed93@gmail.com>
2014-09-02 20:42:48 +0530

    Add tests for cli and viralize

--- test_twit.py
+++
@@ -1,8 +0,0 @@
-from channels import twit
-import pytest
-@pytest.fixture(autouse=True)
-def test_twitter_publish():
-    monkeypatch.delattr("requests.session.Session.request")
-    data={'message':'Hello','channel':'Twitter'}
-    success = twit.twitter_publish(data)
-    assert success == 1

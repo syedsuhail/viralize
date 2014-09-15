e62e724b617ec3bfccf3afcf397c32db90e18c07
Rahul KP <36rahu@users.noreply.github.com>
2014-09-16 02:14:13 +0530

    Update test_cli.py

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,7 +1,6 @@
 import cli
 import sys
 import StringIO
-from binascii import hexlify
 import pytest
 
 def raw_input(prompt):
@@ -13,8 +12,8 @@ def getpass(prompt):
 
 
 def test_get_user_data():
-    data= cli.get_user_data('viral.ini')
-    assert data[0]['channel']=='facebook'
+    data = cli.get_user_data('viral.ini')
+    assert data[0]['channel'] == 'facebook'
     
     error = cli.get_user_data('hello')
     assert error == []
@@ -26,8 +25,8 @@ def test_get_user_data():
   
     sys.stdout = old_stdout
     
-    with pytest.raises(Exception):
-        cli.get_user_data('viral_fail.ini')
+    #with pytest.raises(Exception):
+     #   cli.get_user_data('viral_fail.ini')
        # assert s.getvalue() == "In section facebook, message value is empty\n"
     
     
@@ -40,11 +39,3 @@ def test_status():
     cli.status(results)
     sys.stdout = old_stdout
     assert s.getvalue() == "Twitter Hi\n"
-
-def test_get_username_pass():
-    service='abc'
-    cli.raw_input=raw_input
-    cli.getpass = getpass
-    username,password=cli.get_username_pass('hello')       
-    assert username == hexlify("ssuhail")
-    assert password == hexlify('Tech')

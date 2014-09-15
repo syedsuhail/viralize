4eaf7d60da3838314e8262d4141e24037f30b41b
Rahul KP <36rahu@users.noreply.github.com>
2014-09-16 02:16:03 +0530

    Update and rename test_viralize.py to test_controller.py

---
+++ tests/test_controller.py
@@ -0,0 +1,15 @@
+import controller
+
+
+def test_get_channels():
+    channels = controller.get_channels('channels/')
+    assert channels[0].__name__ == 'channels.facebook_publish'
+
+
+
+
+def test_viralize():
+    data=[{'channel':'test','message':'hello'}]
+    
+    results = controller.viralize(data)
+    assert results == {}

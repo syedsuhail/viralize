ac99f3faac4792393dca97ba7369d1315281536e
Rahul KP <36rahu@users.noreply.github.com>
2014-09-16 02:24:32 +0530

    updated with error checking

--- channels/facebook_publish.py
+++ channels/facebook_publish.py
@@ -1,21 +1,33 @@
 import fbconsole
+import controller
 
-
+'''Recives tokens from the credential file and trying to post on facebook account and return the status message'''
 def publish(data):
     fbconsole.AUTH_SCOPE=['publish_stream']
     try:
         fbconsole.authenticate()
     except Exception:
         return 'Error: Could not authenticate'
+    
+
+    if 'message' in data:
+        if data['message'] != '':
+            message = data['message']
+        else:
+            msg = "Do you want to continue as message in facebook as empty(yes/no):"
+            request = "Twitter message"
+            y,value = controller.warning(msg,request)
+            if  y == 'no' or y == 'NO' or y == 'No':
+                message = value
+            elif y == 'yes' or y == 'YES' or y == 'Yes':
+                return 'Facebook Cannot post the blank message'
+            else:
+                return 'Wrong option is given'
+    else: 
+        return 'The input in Facebook (message) may be wrong. Check your input methode'
+
     try:
-        print data['message']
-#        fbconsole.post('/me/feed', {'source':'Hello'})
+        fbconsole.post('/me/feed', {'message':message})
         return 'Successfully posted message'
     except Exception:
         return 'Error: Could not post message'
-
-#logging
-#git filter-branch --tree 
-#another object config
-#too many . files
-#return values

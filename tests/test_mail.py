d3c2fbaaab8d3b8bc70354cb36ed63a8563e714d
Rahul KP <36rahu@users.noreply.github.com>
2014-09-18 00:44:15 +0530

    updated with raw_input testing

--- tests/test_mail.py
+++ tests/test_mail.py
@@ -1,14 +1,15 @@
 from channels import email_publish
 import ConfigParser
 import os
-
+import __builtin__
+import getpass
 
 mail = email_publish.mail_credential
 assert mail == ".credentials"
 
 def test_if_section():
-    addres = "amvarish"
-    pasw = "amvarish"
+    addres = "hai"
+    pasw = "hai"
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
     if not cfg.has_section('mail'):
@@ -29,18 +30,26 @@ def test_if_section():
         cfg.write(configfile)
     
 
-def test_if_not_section():
+def test_if_not_section(monkeypatch):
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     ab,cd = email_publish.get_value()
     ab = ab.decode('base64','strict');
     cd = cd.decode('base64','strict');
 
-    assert ab == "amvarish"
-    assert cd == "amvarish"
+    assert ab == "hai"
+    assert cd == "hai"
 
 
-def test_if_file_not_exixt():
+def test_if_file_not_exixt(monkeypatch):
     if os.path.exists(mail):
         os.remove(mail)
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
@@ -48,30 +57,34 @@ def test_if_file_not_exixt():
     bc = cfg.get('mail', 'password')
     ab = ab.decode('base64','strict');
     bc = bc.decode('base64','strict');
-    assert ab == "amvarish"
-    assert bc == "amvarish"
+    assert ab == "hai"
+    assert bc == "hai"
 
 
-def test_if_option_not_exixt_in_file():
+def test_if_option_not_exixt_in_file(monkeypatch):
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
     if cfg.has_section('mail'):
         cfg.remove_section('mail')
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
     ab = cfg.get('mail', 'mailaddres')
     bc = cfg.get('mail', 'password')
-    assert ab == "YW12YXJpc2g="
-    assert bc == "YW12YXJpc2g="
+    assert ab == "aGFp"
+    assert bc == "aGFp"
     if os.path.exists(mail):
         os.remove(mail)
 
-def test_if_option_exist_in_file():
-    addres = 'YW12YXJpc2g='
-    pasw = 'YW12YXJpc2g='
+def test_if_option_exist_in_file(monkeypatch):
+    addres = 'aGFp'
+    pasw = 'aGFp'
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
     if not cfg.has_section('mail'):
@@ -80,6 +93,10 @@ def test_if_option_exist_in_file():
         cfg.set('mail', 'password', pasw)
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
     cfg.read(mail)
@@ -108,52 +125,80 @@ def object_has_created():
         os.remove(mail)
 
 
-def test_to_address():
+def test_to_address(monkeypatch):
     dict1 = {'toac': 'abc', 'message':'abcd'}
+    def mock_raw_input(*args, **kwargs):
+        return "amvarish";
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
     assert mes == 'The input in mail (to address) may be wrong. Check your input methode'
     if os.path.exists(mail):
         os.remove(mail)
 
-def test_to_address_empty():
+def test_to_address_empty(monkeypatch):
     dict1 = {'to':'', 'message':'abcd'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
     assert mes == 'The to address could not create as empty'
     if os.path.exists(mail):
         os.remove(mail)
 
-def test_message():
+def test_message(monkeypatch):
     dict1 = {'to': 'abc', 'messsage':'abcd'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
     assert mes == 'The input in mail (message) may be wrong. Check your input methode'
     if os.path.exists(mail):
         os.remove(mail)
 
 
-def test_message_empty():
+def test_message_empty(monkeypatch):
     dict1 = {'to': 'abc', 'message':'', 'subject':'hjgsjhbs'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
-    assert mes == 'could not authenticate'
+    assert mes == 'Given option is wrong:'
     if os.path.exists(mail):
         os.remove(mail)
 
-def test_subject():
+def test_subject(monkeypatch):
     dict1 = {'to': 'abc', 'message':'abcd','subjbect':'abcd'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
     assert mes == 'The input in mail (subject)may be wrong. Check your input methode'
     if os.path.exists(mail):
         os.remove(mail)
 
 
-def test_subject_empty():
+def test_subject_empty(monkeypatch):
     dict1 = {'to': 'abc', 'message':'abcd','subject':''}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
-    assert mes == 'could not authenticate'
+    assert mes == 'Given option is wrong:'
     if os.path.exists(mail):
         os.remove(mail)
 
-def test_canot_login():
+def test_canot_login(monkeypatch):
     dict1 = {'to': 'abc', 'message':'abcd','subject':'hjgsjhbs'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = email_publish.publish(dict1)
     assert mes == 'could not authenticate'
     if os.path.exists(mail):

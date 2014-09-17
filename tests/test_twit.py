d42ffdc64790ebeef854d596384a112f8f5bf6b1
Rahul KP <36rahu@users.noreply.github.com>
2014-09-18 01:27:58 +0530

    Delete test_twit.py

--- tests/test_twit.py
+++
@@ -1,9 +0,0 @@
-from channels import twitter_publish
-import pytest
-
-@pytest.fixture(autouse=True)
-def test_twitter_publish():
-    monkeypatch.delattr("requests.session.Session.request")
-    data={'message':'Hello','channel':'Twitter'}
-    success = twit.twitter_publish(data)
-    assert success == 1

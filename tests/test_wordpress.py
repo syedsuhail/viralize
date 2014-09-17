656ffc6e610be017531e44283be8ef46355bb057
Rahul KP <36rahu@users.noreply.github.com>
2014-09-18 00:08:03 +0530

    updated with raw_input testing

--- tests/test_wordpress.py
+++ tests/test_wordpress.py
@@ -1,6 +1,8 @@
 from channels import wordpress_publish
 import ConfigParser
 import os
+import __builtin__
+import getpass
 from wordpress_xmlrpc import Client, WordPressPost
 
 
@@ -33,20 +35,28 @@ def test_if_section():
         cfg.write(configfile)
     
 
-def test_if_not_section():
+def test_value_not_exist(monkeypatch):
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     ab,bc,cd = wordpress_publish.get_value()
     ab = ab.decode('base64','strict');
     bc = bc.decode('base64','strict');
     cd = cd.decode('base64','strict');
+    
+    assert bc == "yolo"
+    assert ab == "yolo" + "/xmlrpc.php"
+    assert cd == "yolo"
 
-    assert ab == "amvarish" + "/xmlrpc.php"
-    assert bc == "amvarish"
-    assert cd == "amvarish"
 
-
-def test_if_file_not_exixt():
+def test_if_file_not_exixt(monkeypatch):
     if os.path.exists(wordpress):
         os.remove(wordpress)
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     wordpress_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
     cfg.read(wordpress)
@@ -56,27 +66,35 @@ def test_if_file_not_exixt():
     ab = ab.decode('base64','strict');
     bc = bc.decode('base64','strict');
     cd = cd.decode('base64','strict');
-    assert ab == "amvarish/xmlrpc.php"
-    assert bc == "amvarish"
-    assert cd == "amvarish"
+    assert ab == "yolo/xmlrpc.php"
+    assert bc == "yolo"
+    assert cd == "yolo"
 
 
-def test_if_option_not_exixt_in_file():
+def test_if_option_not_exixt_in_file(monkeypatch):
     cfg = ConfigParser.RawConfigParser()
     cfg.read(wordpress)
     if cfg.has_section('Wordpress'):
         cfg.remove_section('Wordpress')
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     wordpress_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
     cfg.read(wordpress)
     ab = cfg.get('Wordpress', 'Wordpressaddres')
     bc = cfg.get('Wordpress', 'password')
     cd = cfg.get('Wordpress', 'username')
-    assert ab == "YW12YXJpc2gveG1scnBjLnBocA=="
-    assert bc == "YW12YXJpc2g="
-    assert cd == "YW12YXJpc2g="
+    ab = ab.decode('base64','strict');
+    bc = bc.decode('base64','strict');
+    cd = cd.decode('base64','strict');
+    assert ab == "yolo/xmlrpc.php"
+    assert bc == "yolo"
+    assert cd == "yolo"
+
     if os.path.exists(wordpress):
         os.remove(wordpress)
 
@@ -106,7 +124,7 @@ def test_if_option_exist_in_file():
         os.remove(wordpress)
 
 def object_has_created():
-    addres = 'ajb,mcxnndv'
+    addres = 'ajbmcxnndv'
     pasw = 'kjfkjnsdkjnd'
     user = 'DJNkjndkla'
     cfg = ConfigParser.RawConfigParser()
@@ -143,32 +161,48 @@ def object_not_created():
         os.remove(wordpress)
     
 
-def test_tittle():
+def test_tittle(monkeypatch):
     dict1 = {'title': 'wordpress_publish post throuh viralise','message':'hajab'}
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = wordpress_publish.publish(dict1)
     assert mes == 'The input in wordpress (tittle) may be wrong. Check your input methode'
     if os.path.exists(wordpress):
         os.remove(wordpress)
 
 
-def test_tittle_empty():
-    dict1 = {'tittle': '','message':'dskjnjs'}
+def test_tittle_empty(monkeypatch):
+    dict1 = {'tittle':'','message':'dskjnjs'}
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = wordpress_publish.publish(dict1)
-    assert mes == 'Could not publish'
+    assert mes == 'Given option is wrong:'
     if os.path.exists(wordpress):
         os.remove(wordpress)
 
 
-def test_message():
+def test_message(monkeypatch):
     dict1 = {'tittle': 'abc', 'messssage': 'This a wordpress_publish post from my app(viralise)'}
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = wordpress_publish.publish(dict1)
     assert mes == 'The input in wordpress (message) may be wrong. Check your input methode'
     if os.path.exists(wordpress):
         os.remove(wordpress)
 
 
-def test_message():
+def test_message_2(monkeypatch):
     dict1 = {'tittle': 'abc', 'message':''}
+    def mock_raw_input(*args, **kwargs):
+        return 'yolo';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
     mes = wordpress_publish.publish(dict1)
     assert mes == 'The message in wordpress could not create as empty'
     if os.path.exists(wordpress):

0bd85b955e97dc69900978ba3166a0c6b1e33245
Rahul KP <36rahu@users.noreply.github.com>
2014-09-18 01:11:11 +0530

    updated with raw_input testing and mocking fbconsole

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -1,41 +1,40 @@
 import ConfigParser
 import os
+import __builtin__
+import fbconsole
 from channels import facebook_publish
 
-MY_TWITTER_CREDS = os.path.expanduser('.fb_access_token')
 
-def test_reading():
-    abc = 'helllo'
-    cfg = ConfigParser.RawConfigParser()
-    cfg.read('.fb_access_token')
-    if not cfg.has_section('abcd'):
-        cfg.add_section('abcd')
-        cfg.set('abcd', 'akjjkan', abc)
-    with open('.fb_access_token', 'wb') as configfile:
-        cfg.write(configfile)
+
+def test_reading(monkeypatch):
     data={'channel':'test','message':'hello'}
+    def mock_raw_input(*args, **kwargs):
+        return 'yes';
+    monkeypatch.setattr(fbconsole, 'authenticate', mock_raw_input)
     ab = facebook_publish.publish(data)
-    assert ab == 'Error: Could not authenticate' 
-    if os.path.exists('.fb_access_token'):
-        os.remove('.fb_access_token')
+    assert ab == 'Error: Could not post message' 
 
 
-def test_message_null():
-    fh = open(MY_TWITTER_CREDS,"w")
-    fh.write('{"access_token": "CAACjeiZB6FgIBAG5JZC5jQMQCl5KKHrqduQLrcV801t3EpHnx7LAZBBZBk0AYM5j36ywmCgQTh4M1fXcT2DxxhIFIpZCkzRVZCXlEbaqN4fJ3rdmG1OYIEg5cvvDYoGrPB0X8BwddVZCZC8br7pUDjZCLvmZBQaZC6GTx32I4swfuZCggwEAD0xikU8HVh9ZAMPHRjc5p7ZAomnAidEIwH86Td6mxF", "scope": ["publish_stream"], "expires_at": 1410814800}')
-    fh.close()
+def test_message_null(monkeypatch):
     data={'channel':'test'}
+    def mock_raw_input(*args, **kwargs):
+        return 'yes';
+    def mock_raw_input(*args, **kwargs):
+        return 'yes';
+    monkeypatch.setattr(fbconsole, 'authenticate', mock_raw_input)
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     ab = facebook_publish.publish(data)
     assert ab == 'The input in Facebook (message) may be wrong. Check your input methode' 
-    if os.path.exists('.fb_access_token'):
-        os.remove('.fb_access_token')
     
-def test_message_empty():
-    fh = open(MY_TWITTER_CREDS,"w")
-    fh.write('{"access_token": "CAACjeiZB6FgIBAG5JZC5jQMQCl5KKHrqduQLrcV801t3EpHnx7LAZBBZBk0AYM5j36ywmCgQTh4M1fXcT2DxxhIFIpZCkzRVZCXlEbaqN4fJ3rdmG1OYIEg5cvvDYoGrPB0X8BwddVZCZC8br7pUDjZCLvmZBQaZC6GTx32I4swfuZCggwEAD0xikU8HVh9ZAMPHRjc5p7ZAomnAidEIwH86Td6mxF", "scope": ["publish_stream"], "expires_at": 1410814800}')
-    fh.close()
+    
+def test_message_empty(monkeypatch):
     data={'channel':'test','message':''}
+    def mock_raw_input(*args, **kwargs):
+        return 'yes';
+    def mock_raw_input(*args, **kwargs):
+        return 'yes';
+    monkeypatch.setattr(fbconsole, 'authenticate', mock_raw_input)
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     ab = facebook_publish.publish(data)
     assert ab == 'Facebook Cannot post the blank message' 
-    if os.path.exists('.fb_access_token'):
-        os.remove('.fb_access_token')
+

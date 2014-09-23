cb361646431d7aed2f433bae77b2d18854c2009b
Rahul KP <36rahu@users.noreply.github.com>
2014-09-24 00:48:03 +0530

    removed user intractive parts

--- channels/facebook_publish.py
+++ channels/facebook_publish.py
@@ -15,15 +15,7 @@ def publish(data):
         if data['message'] != '':
             message = data['message']
         else:
-            msg = "Do you want to continue as message in facebook as empty(yes/no):"
-            request = "Facebook message"
-            y,value = controller.warning(msg,request)
-            if  y == 'no' or y == 'NO' or y == 'No':
-                message = value
-            elif y == 'yes' or y == 'YES' or y == 'Yes':
-                return 'Facebook Cannot post the blank message'
-            else:
-                return 'Wrong option is given'
+            return 'Facebook Cannot post the blank message'
     else: 
         return 'The input in Facebook (message) may be wrong. Check your input methode'
 

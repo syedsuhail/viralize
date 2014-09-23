26bebb077e937edc1f8857b9a49d3c5416fabc74
Rahul KP <36rahu@users.noreply.github.com>
2014-09-24 00:56:36 +0530

    Update test_facebook.py

--- tests/test_facebook.py
+++ tests/test_facebook.py
@@ -17,24 +17,17 @@ def test_reading(monkeypatch):
 
 def test_message_null(monkeypatch):
     data={'channel':'test'}
-    def mock_raw_input(*args, **kwargs):
+    def mock_fb():
         return 'yes';
-    def mock_raw_input(*args, **kwargs):
-        return 'yes';
-    monkeypatch.setattr(fbconsole, 'authenticate', mock_raw_input)
-    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(fbconsole, 'authenticate', mock_fb)
     ab = facebook_publish.publish(data)
     assert ab == 'The input in Facebook (message) may be wrong. Check your input methode' 
     
-    
 def test_message_empty(monkeypatch):
     data={'channel':'test','message':''}
-    def mock_raw_input(*args, **kwargs):
+    def mock_fb():
         return 'yes';
-    def mock_raw_input(*args, **kwargs):
-        return 'yes';
-    monkeypatch.setattr(fbconsole, 'authenticate', mock_raw_input)
-    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(fbconsole, 'authenticate', mock_fb)
     ab = facebook_publish.publish(data)
     assert ab == 'Facebook Cannot post the blank message' 
 

1ee2704e7474512d2c0eff203302fde1600d5d7f
Rahul KP <36rahu@users.noreply.github.com>
2014-09-24 00:59:04 +0530

    Update test_wordpress.py

--- tests/test_wordpress.py
+++ tests/test_wordpress.py
@@ -1,209 +1,191 @@
-from channels import wordpress_publish
+from channels import email_publish
 import ConfigParser
 import os
 import __builtin__
 import getpass
-from wordpress_xmlrpc import Client, WordPressPost
 
-
-wordpress = wordpress_publish.Wordpress_credential
-assert wordpress == ".credentials"
+mail = email_publish.mail_credential
+assert mail == ".credentials"
 
 def test_if_section():
-    addres = "amvarish"
-    pasw = "amvarish"
-    user = "amvarish"
+    addres = "hai"
+    pasw = "hai"
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if not cfg.has_section('Wordpress'):
-        cfg.add_section('Wordpress')
-        cfg.set('Wordpress', 'Wordpressaddres', addres)
-        cfg.set('Wordpress', 'password', pasw)
-        cfg.set('Wordpress', 'username', user)
+    cfg.read(mail)
+    if not cfg.has_section('mail'):
+        cfg.add_section('mail')
+        cfg.set('mail', 'mailaddres', addres)
+        cfg.set('mail', 'password', pasw)
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
-    ab,bc,cd = wordpress_publish.get_value()
+    ab,cd = email_publish.get_value()
     assert ab == addres
-    assert bc == user
     assert cd == pasw
     
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if cfg.has_section('Wordpress'):
-        cfg.remove_section('Wordpress')
+    cfg.read(mail)
+    if cfg.has_section('mail'):
+        cfg.remove_section('mail')
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
     
 
-def test_value_not_exist(monkeypatch):
+def test_if_not_section(monkeypatch):
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
+    def mock_getpass(*args, **kwargs):
+        return 'hai';
+
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
-    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    ab,bc,cd = wordpress_publish.get_value()
+    monkeypatch.setattr(getpass, 'getpass', mock_getpass)
+    ab,cd = email_publish.get_value()
     ab = ab.decode('base64','strict');
-    bc = bc.decode('base64','strict');
     cd = cd.decode('base64','strict');
-    
-    assert bc == "yolo"
-    assert ab == "yolo" + "/xmlrpc.php"
-    assert cd == "yolo"
+
+    assert ab == "hai"
+    assert cd == "hai"
 
 
 def test_if_file_not_exixt(monkeypatch):
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
+    if os.path.exists(mail):
+        os.remove(mail)
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    wordpress_publish.initialise()
+    email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    ab = cfg.get('Wordpress', 'Wordpressaddres')
-    bc = cfg.get('Wordpress', 'password')
-    cd = cfg.get('Wordpress', 'username')
+    cfg.read(mail)
+    ab = cfg.get('mail', 'mailaddres')
+    bc = cfg.get('mail', 'password')
     ab = ab.decode('base64','strict');
     bc = bc.decode('base64','strict');
-    cd = cd.decode('base64','strict');
-    assert ab == "yolo/xmlrpc.php"
-    assert bc == "yolo"
-    assert cd == "yolo"
+    assert ab == "hai"
+    assert bc == "hai"
 
 
 def test_if_option_not_exixt_in_file(monkeypatch):
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if cfg.has_section('Wordpress'):
-        cfg.remove_section('Wordpress')
+    cfg.read(mail)
+    if cfg.has_section('mail'):
+        cfg.remove_section('mail')
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    wordpress_publish.initialise()
+    email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    ab = cfg.get('Wordpress', 'Wordpressaddres')
-    bc = cfg.get('Wordpress', 'password')
-    cd = cfg.get('Wordpress', 'username')
-    ab = ab.decode('base64','strict');
-    bc = bc.decode('base64','strict');
-    cd = cd.decode('base64','strict');
-    assert ab == "yolo/xmlrpc.php"
-    assert bc == "yolo"
-    assert cd == "yolo"
+    cfg.read(mail)
+    ab = cfg.get('mail', 'mailaddres')
+    bc = cfg.get('mail', 'password')
+    assert ab == "aGFp"
+    assert bc == "aGFp"
+    if os.path.exists(mail):
+        os.remove(mail)
 
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
-
-def test_if_option_exist_in_file():
-    addres = 'YW12YXJpc2gveG1scnBjLnBocA=='
-    pasw = 'YW12YXJpc2g='
-    user = 'YW12YXJpc2g='
+def test_if_option_exist_in_file(monkeypatch):
+    addres = 'aGFp'
+    pasw = 'aGFp'
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if not cfg.has_section('Wordpress'):
-        cfg.add_section('Wordpress')
-        cfg.set('Wordpress', 'Wordpressaddres', addres)
-        cfg.set('Wordpress', 'password', pasw)
-        cfg.set('Wordpress', 'username', user)
+    cfg.read(mail)
+    if not cfg.has_section('mail'):
+        cfg.add_section('mail')
+        cfg.set('mail', 'mailaddres', addres)
+        cfg.set('mail', 'password', pasw)
     with open('.credentials', 'wb') as configfile:
         cfg.write(configfile)
-    wordpress_publish.initialise()
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
+    email_publish.initialise()
     cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    ab = cfg.get('Wordpress', 'Wordpressaddres')
-    bc = cfg.get('Wordpress', 'password')
-    cd = cfg.get('Wordpress', 'username')
+    cfg.read(mail)
+    ab = cfg.get('mail', 'mailaddres')
+    bc = cfg.get('mail', 'password')
     assert ab == addres
     assert bc == pasw
-    assert cd == user
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
-
-def object_has_created():
-    addres = 'ajbmcxnndv'
-    pasw = 'kjfkjnsdkjnd'
-    user = 'DJNkjndkla'
-    cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if not cfg.has_section('Wordpress'):
-        cfg.add_section('Wordpress')
-        cfg.set('Wordpress', 'Wordpressaddres', addres)
-        cfg.set('Wordpress', 'password', pasw)
-        cfg.set('Wordpress', 'username', user)
-    with open('.credentials', 'wb') as configfile:
-        cfg.write(configfile)
-    ab = wordpress_publish.initialise()
-    cd = Client(addres, user, pasw)
-    assert ab == cd
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
+    if os.path.exists(mail):
+        os.remove(mail)
 
 
-
-def object_not_created():
-    addres = 'nlknal'
-    user = 'snlksmkms'
-    cfg = ConfigParser.RawConfigParser()
-    cfg.read(wordpress)
-    if not cfg.has_section('Wordpress'):
-        cfg.add_section('Wordpress')
-        cfg.set('Wordpress', 'Wordpressaddres', addres)
-        cfg.set('Wordpress', 'username', user)
-    with open('.credentials', 'wb') as configfile:
-        cfg.write(configfile)
-    ab = wordpress_publish.initialise()
-    assert ab == 'could not authenticate'
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
-    
-
-def test_tittle(monkeypatch):
-    dict1 = {'title': 'wordpress_publish post throuh viralise','message':'hajab'}
+def test_to_address(monkeypatch):
+    dict1 = {'toac': 'abc', 'message':'abcd'}
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return "amvarish";
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    mes = wordpress_publish.publish(dict1)
-    assert mes == 'The input in wordpress (tittle) may be wrong. Check your input methode'
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
+    mes = email_publish.publish(dict1)
+    assert mes == 'The input in mail (to address) may be wrong. Check your input methode'
+    if os.path.exists(mail):
+        os.remove(mail)
 
-
-def test_tittle_empty(monkeypatch):
-    dict1 = {'tittle':'','message':'dskjnjs'}
+def test_to_address_empty(monkeypatch):
+    dict1 = {'to':'', 'message':'abcd'}
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    mes = wordpress_publish.publish(dict1)
-    assert mes == 'Given option is wrong:'
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
-
+    mes = email_publish.publish(dict1)
+    assert mes == 'The to address could not create as empty'
+    if os.path.exists(mail):
+        os.remove(mail)
 
 def test_message(monkeypatch):
-    dict1 = {'tittle': 'abc', 'messssage': 'This a wordpress_publish post from my app(viralise)'}
+    dict1 = {'to': 'abc', 'messsage':'abcd'}
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    mes = wordpress_publish.publish(dict1)
-    assert mes == 'The input in wordpress (message) may be wrong. Check your input methode'
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
+    mes = email_publish.publish(dict1)
+    assert mes == 'The input in mail (message) may be wrong. Check your input methode'
+    if os.path.exists(mail):
+        os.remove(mail)
 
 
-def test_message_2(monkeypatch):
-    dict1 = {'tittle': 'abc', 'message':''}
+def test_message_empty(monkeypatch):
+    dict1 = {'to': 'abc', 'message':'', 'subject':'hjgsjhbs'}
     def mock_raw_input(*args, **kwargs):
-        return 'yolo';
+        return 'hai';
     monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
     monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
-    mes = wordpress_publish.publish(dict1)
-    assert mes == 'The message in wordpress could not create as empty'
-    if os.path.exists(wordpress):
-        os.remove(wordpress)
+    mes = email_publish.publish(dict1)
+    assert mes == 'could not authenticate'
+    if os.path.exists(mail):
+        os.remove(mail)
+
+def test_subject(monkeypatch):
+    dict1 = {'to': 'abc', 'message':'abcd','subjbect':'abcd'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
+    mes = email_publish.publish(dict1)
+    assert mes == 'The input in mail (subject)may be wrong. Check your input methode'
+    if os.path.exists(mail):
+        os.remove(mail)
+
+
+def test_subject_empty(monkeypatch):
+    dict1 = {'to': 'abc', 'message':'abcd','subject':''}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
+    mes = email_publish.publish(dict1)
+    assert mes == 'could not authenticate'
+    if os.path.exists(mail):
+        os.remove(mail)
+
+def test_canot_login(monkeypatch):
+    dict1 = {'to': 'abc', 'message':'abcd','subject':'hjgsjhbs'}
+    def mock_raw_input(*args, **kwargs):
+        return 'hai';
+    monkeypatch.setattr(__builtin__, 'raw_input', mock_raw_input)
+    monkeypatch.setattr(getpass, 'getpass', mock_raw_input)
+    mes = email_publish.publish(dict1)
+    assert mes == 'could not authenticate'
+    if os.path.exists(mail):
+        os.remove(mail)
